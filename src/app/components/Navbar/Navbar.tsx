52655f6ebfb9c642ac54ea3a9aeae925808a86ad
Sofia Donlucas Bañuelos <71408638+Soff17@users.noreply.github.com>
2024-11-04 12:22:26 -0600

    Adding linkedin

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { AppBar, Toolbar, Box } from "@mui/material";
 import Image from "next/image";
 import logo from "../../assets/logo.png"; // Adjust the path to your logo
-import CustomButton from "../customButton/customButton";
 
 const Navbar = () => {
   return (
@@ -32,17 +31,24 @@ const Navbar = () => {
           />
         </Box>
 
-        {/* Responsive Button */}
+        {/* LinkedIn SVG Icon */}
         <Box>
-          <CustomButton
-            href="/contact"
-            borderColor="#FFC300"
-            textColor="#FFC300"
-            hoverBackgroundColor="#FFC300"
-            hoverTextColor="#F5F5F5"
+          <a
+            href="https://www.linkedin.com/company/neuralfactory/" // Replace with your LinkedIn profile URL
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "#FFC300", display: "flex", alignItems: "center" }}
           >
-            Contacto
-          </CustomButton>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="#FFC300" // Adjust color as needed
+              viewBox="0 0 24 24"
+            >
+              <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.761 0 5-2.239 5-5v-14c0-2.761-2.239-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.268c-.966 0-1.75-.806-1.75-1.8s.784-1.8 1.75-1.8c.967 0 1.75.806 1.75 1.8s-.783 1.8-1.75 1.8zm13.5 11.268h-3v-5.6c0-1.341-.028-3.066-1.868-3.066-1.87 0-2.157 1.461-2.157 2.971v5.695h-3v-10h2.881v1.367h.042c.401-.756 1.379-1.553 2.841-1.553 3.039 0 3.6 2.001 3.6 4.6v5.586z"/>
+            </svg>
+          </a>
         </Box>
       </Toolbar>
     </AppBar>
