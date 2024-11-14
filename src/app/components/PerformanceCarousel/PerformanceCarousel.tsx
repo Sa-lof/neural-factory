b6dc1551d8dbcfb8c8f9037b7f9bf07217588083
salomon <mtzelisalomon@gmail.com>
2024-11-14 11:24:11 -0600

    logo

--- src/app/components/PerformanceCarousel/PerformanceCarousel.tsx
+++ src/app/components/PerformanceCarousel/PerformanceCarousel.tsx
@@ -4,7 +4,7 @@ import CircleIcon from "@mui/icons-material/Circle"; // Importa un ícono circul
 import PerformanceChart from "../PerformanceChart/PerformanceChart";
 import PerformanceChartLargeDataset from "../PerformanceChartLargeDataset/PerformanceChartLargeDataset";
 import PerformanceChartLastDataset from "../PerformanceChartLastDataset/PerformanceChartLastDataset";
-import megaladata from "../../assets/graphs/megaladata.png";
+import mega from "../../assets/graphs/mega.png";
 import Image from "next/image";
 
 const charts = [
@@ -45,18 +45,19 @@ const PerformanceCarousel: React.FC = () => {
         paddingBottom: 15,
       }}
     >
-      <Typography variant="h5" component="h2" gutterBottom>
+      <Typography variant="h5" component="h2" gutterBottom >
         En nuestras soluciones, utilizamos el software de mejor
         <Box
           component="span"
-          sx={{ display: "inline-flex", alignItems: "center" }}
+          sx={{ display: "inline-flex", alignItems: "center", ml: 1, mr: 1 }}
         >
           rendimiento
           <Image
-            src={megaladata.src}
+            src={mega.src}
             alt="Megaladata"
-            width={100}
-            height={200}
+            width={200}
+            height={500}
+            style={{ marginLeft: 10 }}
           />
         </Box>
       </Typography>
